9c4382a6270c4d024c458a3295ad271f590b125d
Artur Markov <artur.markov1860@gmail.com>
2019-10-22 16:10:57 -0700

    styling campgrounds page

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.set("view engine", "ejs");
 var campgrounds = [
     {name: "Salmon Creek", image: "https://s.abcnews.com/images/Travel/gty_camping_kb_140711_16x9_992.jpg"},
     {name: "Mountain Goat's Rest", image: "https://cdn.vox-cdn.com/thumbor/FMUIaXcnBaKK9YqdP8qtxUog150=/0x0:4741x3161/1200x800/filters:focal(1992x1202:2750x1960)/cdn.vox-cdn.com/uploads/chorus_image/image/59535149/shutterstock_625918454.0.jpg"},
+    {name: "Granite Hill", image: "https://www.visitmt.com/binaries/content/gallery/MTOT/responsive/tiles-p/p1/places-to-stay/0013mt_tentrgb_rm_web.jpg"},
+    {name: "Salmon Creek", image: "https://s.abcnews.com/images/Travel/gty_camping_kb_140711_16x9_992.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://cdn.vox-cdn.com/thumbor/FMUIaXcnBaKK9YqdP8qtxUog150=/0x0:4741x3161/1200x800/filters:focal(1992x1202:2750x1960)/cdn.vox-cdn.com/uploads/chorus_image/image/59535149/shutterstock_625918454.0.jpg"},
+    {name: "Granite Hill", image: "https://www.visitmt.com/binaries/content/gallery/MTOT/responsive/tiles-p/p1/places-to-stay/0013mt_tentrgb_rm_web.jpg"},
+    {name: "Salmon Creek", image: "https://s.abcnews.com/images/Travel/gty_camping_kb_140711_16x9_992.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://cdn.vox-cdn.com/thumbor/FMUIaXcnBaKK9YqdP8qtxUog150=/0x0:4741x3161/1200x800/filters:focal(1992x1202:2750x1960)/cdn.vox-cdn.com/uploads/chorus_image/image/59535149/shutterstock_625918454.0.jpg"},
     {name: "Granite Hill", image: "https://www.visitmt.com/binaries/content/gallery/MTOT/responsive/tiles-p/p1/places-to-stay/0013mt_tentrgb_rm_web.jpg"}
 ];
 
